7911acdc363df0c71b11a074171c7b64e500c2ce
artemkugushev <mr.ar4ik33@gmail.com>
2023-04-13 22:54:45 +0300

    Add hw

--- botify/botify/server.py
+++ botify/botify/server.py
@@ -16,6 +16,7 @@ from botify.recommenders.sticky_artist import StickyArtist
 from botify.recommenders.toppop import TopPop
 from botify.recommenders.indexed import Indexed
 from botify.recommenders.contextual import Contextual
+from botify.recommenders.contextual_own import ContextualOwn
 from botify.track import Catalog
 
 import numpy as np
@@ -40,14 +41,22 @@ data_logger = DataLogger(app)
 catalog = Catalog(app).load(
     app.config["TRACKS_CATALOG"], app.config["TOP_TRACKS_CATALOG"], app.config["TRACKS_WITH_DIVERSE_RECS_CATALOG"]
 )
+catalog_own = Catalog(app).load(
+    app.config["OWN_TRACKS_CATALOG"], app.config["TOP_TRACKS_CATALOG"], app.config["TRACKS_WITH_DIVERSE_RECS_CATALOG"]
+)
 catalog.upload_tracks(tracks_redis.connection, tracks_with_diverse_recs_redis.connection)
 catalog.upload_artists(artists_redis.connection)
 catalog.upload_recommendations(recommendations_redis.connection)
 catalog.upload_recommendations(recommendations_ub_redis.connection, "RECOMMENDATIONS_UB_FILE_PATH")
+catalog_own.upload_tracks(tracks_redis.connection, tracks_with_diverse_recs_redis.connection)
+catalog_own.upload_artists(artists_redis.connection)
+catalog_own.upload_recommendations(recommendations_redis.connection)
+catalog_own.upload_recommendations(recommendations_ub_redis.connection, "RECOMMENDATIONS_UB_FILE_PATH")
 
 parser = reqparse.RequestParser()
 parser.add_argument("track", type=int, location="json", required=True)
 parser.add_argument("time", type=float, location="json", required=True)
+history = {}
 
 
 class Hello(Resource):
@@ -69,26 +78,25 @@ class Track(Resource):
 
 class NextTrack(Resource):
     def post(self, user: int):
+        global history
         start = time.time()
 
         args = parser.parse_args()
 
-        # TODO Seminar 6 step 6: Wire RECOMMENDERS A/B experiment
-        treatment = Experiments.RECOMMENDERS.assign(user)
-        if treatment == Treatment.T1:
-            recommender = StickyArtist(tracks_redis.connection, artists_redis.connection, catalog)
-        elif treatment == Treatment.T2:
-            recommender = TopPop(tracks_redis.connection, catalog.top_tracks[:100])
-        elif treatment == Treatment.T3:
-            recommender = Indexed(tracks_redis.connection, recommendations_ub_redis.connection, catalog)
-        elif treatment == Treatment.T4:
-            recommender = Indexed(tracks_redis.connection, recommendations_redis.connection, catalog)
-        elif treatment == Treatment.T5:
-            recommender = Contextual(tracks_redis.connection, catalog)
-        elif treatment == Treatment.T6:
-            recommender = Contextual(tracks_with_diverse_recs_redis.connection, catalog)
+        user_history = history.get(user, {})
+        if not user_history:
+            history[user] = {args.track: args.time}
         else:
-            recommender = Random(tracks_redis.connection)
+            user_history[args.track] = args.time
+            history[user] = user_history
+        # TODO Seminar 6 step 6: Wire RECOMMENDERS A/B experiment
+        treatment = Experiments.CONTEXTUAL.assign(user)
+        if treatment == Treatment.T1:
+            recommender = ContextualOwn(tracks_with_diverse_recs_redis.connection, catalog, history)
+            #recommender = Contextual(tracks_with_diverse_recs_redis.connection, catalog_own)
+        else:
+            recommender = Contextual(tracks_with_diverse_recs_redis.connection, catalog)
+            #recommender = Random(tracks_redis.connection)
 
         recommendation = recommender.recommend_next(user, args.track, args.time)
 
